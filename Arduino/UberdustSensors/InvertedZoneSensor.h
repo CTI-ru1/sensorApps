6e0fb812e6ffe6d71d94fd08e191f683aa491554
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-11-14 11:24:57 +0200

    add new ZoneSensor for the reverse operation

---
+++ Arduino/UberdustSensors/InvertedZoneSensor.h
@@ -0,0 +1,38 @@
+#include <CoapSensor.h>
+
+class zoneSensor : 
+public CoapSensor 
+{
+public:
+  int pin, status;
+  zoneSensor(): 
+  CoapSensor()
+  {
+  }
+
+  zoneSensor(char * name, int pin): 
+  CoapSensor(name)
+  {
+    this->pin = pin;
+    this->status = 0;
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+    this->set_notify_time(30);
+  }
+  void get_value( uint8_t* output_data, size_t* output_data_len)
+  {
+    this->status = digitalRead(this->pin);
+    *output_data_len = sprintf((char *)output_data,"%d",this->status);
+  }
+  void set_value(uint8_t* input_data, size_t input_data_len, uint8_t* output_data, size_t* output_data_len)
+  {
+    this->set(*input_data-0x30);
+    this->changed = true;
+    get_value(output_data,output_data_len);
+  }
+  inline void set(uint8_t value)
+  {
+    this->status = value;
+    digitalWrite(pin, 1-status);
+  }
+};
