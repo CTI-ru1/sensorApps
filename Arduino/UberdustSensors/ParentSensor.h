2f626d5221145781723f963e9a4c80de32f7beaa
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-11-19 23:11:19 +0200

    change parent sensor to get the parent form the routing alg

--- Arduino/UberdustSensors/ParentSensor.h
+++ Arduino/UberdustSensors/ParentSensor.h
@@ -4,14 +4,16 @@ class parentSensor :
 public CoapSensor 
 {
 public:
-  parentSensor(char * name):   
+  parentSensor(char * name,BaseRouting* routing):   
   CoapSensor(name)
   {  
-      this->set_notify_time(300);
+      this->routing = routing;
+      this->set_notify_time(200);
   }
   void get_value( uint8_t* output_data, size_t* output_data_len)
   {
-    *output_data_len = sprintf( (char*)output_data, "0x%x", parent_); 
+    *output_data_len = sprintf( (char*)output_data, "0x%x", routing->parent()); 
   }
-  uint16_t parent_;
+  void check(){}
+  BaseRouting * routing;
 };
