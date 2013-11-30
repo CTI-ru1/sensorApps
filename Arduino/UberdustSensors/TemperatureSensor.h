65689c4a00f1cfa8b1ae34cba9755afea689c92e
Lidia <you@example.com>
2013-11-30 18:35:19 +0200

    fix temperature for TM36

--- Arduino/UberdustSensors/TemperatureSensor.h
+++ Arduino/UberdustSensors/TemperatureSensor.h
@@ -1,4 +1,6 @@
 #include <CoapSensor.h>
+#ifndef TEMPERATURESENSOR
+#define TEMPERATURESENSOR 
 
 class temperatureSensor : 
 public CoapSensor 
@@ -10,7 +12,7 @@ public:
   {
     this->pin = pin;
     this->status = 0;
-    this->set_notify_time(180);
+    this->set_notify_time(10);
   }
   void get_value( uint8_t* output_data, size_t* output_data_len)
   {
@@ -21,10 +23,18 @@ public:
     static unsigned long timestamp = 0;
     if(millis() - timestamp > 5000)
     {
-      int newStatus = analogRead(this->pin)*100;  // read the value from the sensor
+  	//getting the voltage reading from the temperature sensor
+ 	int reading = analogRead(this->pin);  
+ 
+ 	// converting that reading to voltage, for 3.3v arduino use 3.3
+ 	float voltage = reading * 5.0;
+ 	voltage /= 1024.0; 
 
-      newStatus = ((newStatus/1024)* 5000)/10/100;  
-      if(newStatus != this->status)
+	 // now print out the temperature
+ 	float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+ 	int newStatus=temperatureC;
+       if(newStatus != this->status)
       {
         this->status = newStatus;
       }
@@ -32,3 +42,4 @@ public:
     }
   }
 };
+#endif
