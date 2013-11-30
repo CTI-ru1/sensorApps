dcbfabe53eb190c18e408385aef1a1b156e31a94
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-11-30 18:38:28 +0200

    change Random

--- Arduino/UberdustSensors/RandomSensor.h
+++ Arduino/UberdustSensors/RandomSensor.h
@@ -8,12 +8,20 @@ public:
   CoapSensor(name)
   {  
       this->limit = limit;
+      this->status = 0;
       this->set_notify_time(10);
   }
   void get_value( uint8_t* output_data, size_t* output_data_len)
   {
-    *output_data_len = sprintf( (char*)output_data, "%d", rand()%limit); 
+    *output_data_len = sprintf( (char*)output_data, "%d", this->status); 
   }
-  uint16_t parent_;
+  void check(){
+    static unsigned long timestamp = 0;
+    if(millis() - timestamp > 5000)
+    {
+      this->status=rand()%limit;
+    }
+  }
   int limit;
+  int status;
 };
